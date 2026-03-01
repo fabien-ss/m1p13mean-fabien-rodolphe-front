76817d802b42c7f3913d72506257676dd9265a8c
01KingMaker <fabrakoto0987@gmail.com>
2026-03-01 16:31:43 +0300

    feat: replace url backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000'
+    production: true,
+    apiUrl: 'https://m1p13mean-fabien-rodolphe-back-1.onrender.com'
 };
